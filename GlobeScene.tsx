22ab50bf62b391e533886416af4aba797900e718
parinox <x89c8zaid@gmail.com>
2026-05-25 23:00:40 +0530

    Update GlobeScene.tsx

--- GlobeScene.tsx
+++ GlobeScene.tsx
@@ -1,7 +1,1 @@
-export default function GlobeScene() {
-  return (
-    <div className="w-full h-full flex items-center justify-center text-green-400">
-      Tactical Globe Engine Initializing...
-    </div>
-  );
-}
+
